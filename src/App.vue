1e0caab955e1ed4979b6c3732d82cd72b08a1f4e
Aziz Mejri <zizou.aziz101@gmail.com>
2021-09-28 15:06:51 +0100

    Update NavBar

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,7 @@
       <div class="d-flex align-center">
         <v-toolbar-title>Torre</v-toolbar-title>
       </div>
-
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
